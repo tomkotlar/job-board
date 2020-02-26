bae7b35f1d2e573cdc53b1e27f6390d809534d9a
tomkotlar <t.kotlar@yahoo.com>
2020-02-26 20:59:55 +0000

    adding github-jobs file

---
+++ server/github-jobs/github-jobs.js
@@ -0,0 +1,29 @@
+const axios = require("axios").default
+
+const instance = axios.create({
+  baseURL: "https://jobs.github.com/positions.json"
+})
+
+const fetchGihubJobs = async () => {
+  let allVacancies = []
+  let count = 1
+  let currentPage = 0
+
+  while (count > 0) {
+    await instance
+      .get(`?page=${currentPage}`)
+      .then(response => {
+        let jobs = response.data
+        allVacancies.push(...jobs)
+        count = jobs.length
+        currentPage++
+        console.log(count, "--------")
+      })
+      .catch(err => console.log(err, "this is errror ____________"))
+  }
+  console.log(allVacancies.length, "{------- JOBS")
+}
+
+fetchGihubJobs()
+
+module.exports = fetchGihubJobs
