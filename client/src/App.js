eb754a786bf2f3b907cfe761dc624fcd52424559
tomkotlar <t.kotlar@yahoo.com>
2020-02-29 23:15:24 +0000

    cheanging the app cromponent to class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,55 @@
-import React, { useEffect, useState } from "react"
+import React, { Component } from "react"
 import "./App.css"
 import { JobsList } from "./Components/JobList/JobsList"
-import { Segment, Icon, Button, Header, Loader, Dimmer } from "semantic-ui-react"
+import { SearchBox } from "./Components/SearchBox/SearchBox"
+import { Loader, Dimmer } from "semantic-ui-react"
 
 const api = "http://localhost:9090/jobs"
 
-async function fetchVacancies(update) {
-  try {
-    const response = await fetch(api)
-    const jobList = await response.json()
-    console.log({ jobList })
-    update(jobList)
-  } catch (err) {
-    console.log(err)
+class App extends Component {
+  state = {
+    error: null,
+    vacanciesList: [],
+    searchPosition: ""
   }
-}
 
-function App() {
-  const [vacanciesList, updateVacancies] = useState([])
+  async componentDidMount() {
+    try {
+      const response = await fetch(api)
+      const jobList = await response.json()
+      this.setState({ vacanciesList: jobList })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-  useEffect(() => {
-    fetchVacancies(updateVacancies)
-  }, [])
+  handleSearch = event => {
+    this.setState({ searchPosition: event.target.value })
+  }
 
-  return (
-    <div className="segment">
-      <Segment placeholder padded>
-        <Header icon>
-          <Icon name="search" />
-          Looking for Dev Jobs
-        </Header>
-        <Segment.Inline>
-          <Button primary>Clear Query</Button>
-          <Button>Add Document</Button>
-        </Segment.Inline>
-      </Segment>
-      {!vacanciesList.length ? (
-          <Dimmer active >
-          <Loader size='massive'>Loading</Loader>
-        </Dimmer>
-      ) : (
-        <div className="Jobs">
-          <JobsList vacancies={vacanciesList} />
-        </div>
-      )}
-    </div>
-  )
+  render() {
+    const { vacanciesList, searchPosition } = this.state
+    const filterJobs = vacanciesList.filter(job =>
+      job.title.toLowerCase().includes(searchPosition.toLowerCase())
+    )
+    return (
+      <div>
+        <SearchBox
+          handleSearch={this.handleSearch}
+          placeholder="Search for position"
+        />
+        {!vacanciesList.length ? (
+          <Dimmer active>
+            <Loader size="massive">Loading</Loader>
+          </Dimmer>
+        ) : (
+          <div className="Jobs">
+            <JobsList vacancies={filterJobs} />
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
